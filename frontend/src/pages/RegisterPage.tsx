5df8b49ae18631a43c111bddea23bae896bddc78
miliracle <nguyennhathan126@gmail.com>
2025-11-09 00:36:30 +0700

    chore: login/signup page

--- frontend/src/pages/RegisterPage.tsx
+++ frontend/src/pages/RegisterPage.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { Link } from '@tanstack/react-router';
 import { useSelector } from 'react-redux';
 import { Button } from '../components/ui/button';
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '../components/ui/card';
+import { Card, CardContent } from '../components/ui/card';
 import { Input } from '../components/ui/input';
 import { Label } from '../components/ui/label';
 import { useRegister } from '../hooks/mutations/useAuthMutations';
